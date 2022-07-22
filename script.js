1784e47b48a21570b0348db527a2a87b99acea42
Cesar Augusto <69799713+gabrielleduarte01@users.noreply.github.com>
2022-07-21 22:46:25 -0300

    commit

--- script.js
+++ script.js
@@ -1,20 +1,129 @@
 // selecionar os elementos de ação 
-const inputElement = document.querySelector(".new-task-input")
-const addTaskButton = document.querySelector(".new-task-button")
+const inputElement = document.querySelector(".new-task-input");
+const addTaskButton = document.querySelector(".new-task-button");
 
-const validateInput = () => inputElement.value.trim().length > 0 //se for maior que 0 true (trim espaços em branco)
+const tasksContainer = document.querySelector(".tasks-container");
+
+
+const validateInput = () => inputElement.value.trim().length > 0; //se for maior que 0 true (trim espaços em branco)
+
 const handleAddTask = () => {
-    const inputIsValid = validateInput() // checar se o input é valido
-    if(!inputIsValid) { // se o input n for valido
-        return inputElement.classList.add("error") //validação input
+    const inputIsValid = validateInput(); // checar se o input é valido
+
+    console.log(inputIsValid);
+
+    if (!inputIsValid) { // se o input n for valido
+        return inputElement.classList.add("error"); //validação input
     }
-}
+
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = inputElement.value;
+
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("fa-regular");
+    deleteItem.classList.add("fa-trash-can");
+
+    deleteItem.addEventListener("click", () => handleDeleteClick(taskItemContainer, taskContent));
+    
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+
+    updateLocalStorage();
+};
+ 
+const handleClick = (taskContent) => {
+    const tasks = tasksContainer.childNodes; // tds os filhos dos tasks container    
+
+    for (const task of tasks) { // para cada tarefa da lista
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+        if (currentTaskIsBeingClicked) { //task = div task-item
+            task.firstChild.classList.toggle("completed");
+        }
+    }
+
+    updateLocalStorage();
+};
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+    const tasks  = tasksContainer.childNodes;
+    
+    for (const task of tasks) {
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+        
+        if (currentTaskIsBeingClicked) {
+            taskItemContainer.remove();
+        }
+    }
+
+    updateLocalStorage();
+};
 
 const handleInputChange = () => { // remover erro
-    const inputIsValid = validateInput()
-    if(inputIsValid) {
-        return inputElement.classList.remove("error") 
+    const inputIsValid = validateInput();
+
+    if (inputIsValid) {
+        return inputElement.classList.remove("error");
     }
-}
-addTaskButton.addEventListener("click", () => handleAddTask()) // validação input
-inputElement.addEventListener("change", () => handleInputChange()) // remover erro
+};
+
+const updateLocalStorage = () => {
+    const tasks = tasksContainer.childNodes;
+
+    const localStorageTasks = [...tasks].map((task) => {
+        const content = task.firstChild;
+        const isCompleted = content.classList.contains("completed");
+
+        return { description: content.innerText, isCompleted };
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(localStorageTasks));
+};
+
+const refreshTasksUsingLocalStorage = () => {
+    const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!tasksFromLocalStorage) return;
+
+    for (const task of tasksFromLocalStorage) {
+        const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = task.description;
+
+    if (task.isCompleted) {
+        taskContent.classList.add("completed");
+    }
+
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("fa-regular");
+    deleteItem.classList.add("fa-trash-can");
+
+    deleteItem.addEventListener("click", () => handleDeleteClick(taskItemContainer, taskContent));
+    
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+    }
+};
+
+refreshTasksUsingLocalStorage();
+
+addTaskButton.addEventListener("click", () => handleAddTask()); // validação input
+
+inputElement.addEventListener("change", () => handleInputChange()); // remover erro
